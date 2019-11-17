749fcf2aef26dbc51b1335295aa73d315b3e63f3
Dmitrii <39788810+MagicWinnie@users.noreply.github.com>
2019-11-18 02:45:10 +0700

    Update 1.py

--- SecondStage/Programming/1.py
+++ SecondStage/Programming/1.py
@@ -11,8 +11,8 @@ for _ in range(n):
         d[int(num)] = [[x_ice, y_ice, x_drone, y_drone, height]]
 
 def size(height):
-    x = round((4*height)/3, 2)
-    y = round((3*height)/3, 2)
+    x = (4*height)/3
+    y = (3*height)/3
     return x, y
 
 for key, item in zip(d.keys(), d.values()):
@@ -20,28 +20,31 @@ for key, item in zip(d.keys(), d.values()):
     for i in range(len(item)):
         x, y = size(item[i][-1])
         
-        r_x = round(item[i][0] * x / 640, 2)
-        r_y = round(item[i][1] * y / 480, 2)
-        if item[i][0] > 320:
-            real_x = item[i][2] + (r_x-(x//2))
+        r_x = item[i][0] * x / 640
+        r_y = item[i][1] * y / 480
+        if item[i][0] >= 320:
+            real_x = item[i][2] + (r_x-(x/2))
         else:
-            real_x = item[i][2] - ((x//2)-r_x)
-        if item[i][1] > 240:
-            real_y = item[i][3] - ((y//2)-r_y)
+            real_x = item[i][2] - ((x/2)-r_x)
+        if item[i][1] >= 240:
+            real_y = item[i][3] - ((y/2)-r_y)
         else:
-            real_y = item[i][3] + (r_y-(y//2))
+            real_y = item[i][3] + (r_y-(y/2))
+        
         temp_arr[0].append(real_x)
         temp_arr[1].append(real_y)
 
-    d_new[key] = [round(sum(temp_arr[0])/len(temp_arr[0])), round(sum(temp_arr[1])/len(temp_arr[1]))]
+    d_new[key] = [sum(temp_arr[0])/len(temp_arr[0]), sum(temp_arr[1])/len(temp_arr[1])]
 
 
 
-#---------------------------------------------------
 x_x = [[[],[]],[[],[]],[[],[]]]
+
 sorted_ = sorted(d_new.items(), key=lambda kv: kv[1])
+
 counter = 0
 count = 0
+
 for i in range(len(sorted_)):
     if count < 4:
         x_x[counter][0].append(sorted_[i][0])
@@ -58,6 +61,6 @@ for i in range(3):
     for j in range(4):
         res[j][i] = x_x[i][0][3-j]
 
-#---------------------------------------------------
+
 for i in res:
     print(*i)
